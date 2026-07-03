c166401c23094ab3bbb37af800d2faa0041fb466
BGHOLY <1426381719@qq.com>
2026-07-03 19:04:10 +0800

    feat(db): connect mysql with typeorm

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,11 +8,16 @@ import { AppService } from './app.service';
 import { PetModule } from './modules/pet/pet.module';
 import { UserModule } from './modules/user/user.module';
 
+import { databaseConfig } from './config/database.config';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+
+    TypeOrmModule.forRoot(databaseConfig),
+
     PetModule,
     UserModule,
   ],
